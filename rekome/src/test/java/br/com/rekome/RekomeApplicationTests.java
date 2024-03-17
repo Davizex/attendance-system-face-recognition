9abed3f5a9c8d645444117a75be044405d6fd58d
Davi Fernandes <daviifernandes@gmail.com>
2024-03-17 16:39:13 -0300

    git init

---
+++ rekome/src/test/java/br/com/rekome/RekomeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rekome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RekomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
