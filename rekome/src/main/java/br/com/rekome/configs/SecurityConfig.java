2697791ab6a8334e5f7706e3d883b489cffdb68b
Davi Fernandes <daviifernandes@gmail.com>
2024-09-29 15:58:25 -0300

    security config

--- rekome/src/main/java/br/com/rekome/configs/SecurityConfig.java
+++ rekome/src/main/java/br/com/rekome/configs/SecurityConfig.java
@@ -20,6 +20,9 @@ import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.header.writers.CrossOriginEmbedderPolicyHeaderWriter.CrossOriginEmbedderPolicy;
+import org.springframework.security.web.header.writers.CrossOriginOpenerPolicyHeaderWriter.CrossOriginOpenerPolicy;
+import org.springframework.security.web.header.writers.CrossOriginResourcePolicyHeaderWriter.CrossOriginResourcePolicy;
 
 import com.nimbusds.jose.jwk.JWK;
 import com.nimbusds.jose.jwk.JWKSet;
@@ -30,50 +33,55 @@ import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
-	
-	@Value("${APP_PUBLIC_KEY}")
-	private RSAPublicKey publicKey;
-	
-	@Value("${APP_PRIVATE_KEY}")
-	private RSAPrivateKey privateKey;
-	
-	@Bean
-    SecurityFilterChain filterChain(HttpSecurity http, AuthenticationConfiguration authConfig) throws Exception {
-						
-		http.headers((headers) -> headers
-				.frameOptions((frame) -> frame.deny())
-				.contentSecurityPolicy((securityPolice) -> securityPolice.policyDirectives("frame-ancestors 'self'"))
-				.httpStrictTransportSecurity(
-						(sts) -> sts.maxAgeInSeconds(31536000).includeSubDomains(true).preload(true))
-				.referrerPolicy((police) -> police.policy(SAME_ORIGIN)));
-		
-		http
-			.oauth2ResourceServer( oauth -> oauth
-					.jwt(Customizer.withDefaults()))
-			.sessionManagement(session -> session
-					.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-			.csrf(csrf -> csrf.disable());
-		
-		http
-			.authorizeHttpRequests((auth) -> auth
-					.requestMatchers("/auth/login").permitAll()
-					.requestMatchers("/user/create").permitAll()
-					.anyRequest().authenticated());
 
-		
+	@Value("${app.secure.public-key}")
+	private RSAPublicKey publicKey;
+
+	@Value("${app.secure.private-key}")
+	private RSAPrivateKey privateKey;
+
+	@Value("${app.secure.secure-headers}")
+	private boolean isSecureHeaders;
+
+	@Bean
+	SecurityFilterChain filterChain(HttpSecurity http, AuthenticationConfiguration authConfig) throws Exception {
+
+		if (isSecureHeaders) {
+			http.headers((headers) -> headers.frameOptions((frame) -> frame.deny())
+					.contentSecurityPolicy(
+							(securityPolice) -> securityPolice.policyDirectives("frame-ancestors 'self'"))
+					.crossOriginEmbedderPolicy(coep -> coep.policy(CrossOriginEmbedderPolicy.REQUIRE_CORP))
+					.crossOriginResourcePolicy(corp -> corp.policy(CrossOriginResourcePolicy.SAME_ORIGIN))
+					.crossOriginOpenerPolicy(coop -> coop.policy(CrossOriginOpenerPolicy.SAME_ORIGIN))
+					.httpStrictTransportSecurity(
+							(sts) -> sts.maxAgeInSeconds(31536000).includeSubDomains(true).preload(true))
+					.referrerPolicy((police) -> police.policy(SAME_ORIGIN)));
+		}
+
+		http.csrf(csrf -> csrf.disable());
+
+		http.oauth2ResourceServer(oauth -> oauth.jwt(Customizer.withDefaults()))
+				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+		http.authorizeHttpRequests((auth) -> auth
+				.requestMatchers("/auth/login").permitAll()
+				.requestMatchers("/user/create").permitAll()
+				.requestMatchers("/actuator/**").permitAll()
+				.anyRequest().authenticated());
+
 		return http.build();
 	}
-	
+
 	@Bean
 	AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
 		return authConfig.getAuthenticationManager();
 	}
-	
+
 	@Bean
 	JwtDecoder jwtDecoder() {
 		return NimbusJwtDecoder.withPublicKey(publicKey).build();
 	}
-	
+
 	@Bean
 	JwtEncoder jwtEncoder() {
 		JWK jwk = new RSAKey.Builder(this.publicKey).privateKey(privateKey).build();
